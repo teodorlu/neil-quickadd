f7713f54974aa6318b432f10b6bc5a979755a281
Teodor Heggelund <git@teod.eu>
2023-01-26 18:16:10 +0100

    INIT

---
+++ README.md
@@ -0,0 +1,15 @@
+# neil-quickadd
+
+## Installing
+
+To install the IKI CLI,
+
+1. Clone the repo,
+
+        git clone https://github.com/teodorlu/neil-quickadd.git
+        
+2. then install the script with [babashka/bbin][babashka-bbin]
+
+        bbin install ./neil-quickadd/
+
+[babashka-bbin]: https://github.com/babashka/bbin
---
+++ src/teodorlu/neil_quickadd.clj
@@ -0,0 +1,67 @@
+(ns teodorlu.neil-quickadd
+  (:require
+   [babashka.fs :as fs]
+   [clojure.string :as str]
+   [babashka.cli :as cli]))
+
+;; Rationale
+;;
+;; neil dep add requires me to remember coordinates. I /can/ use Ctrl+R to find stuff I've added previously.
+;;
+;; neil-quickadd approaches it differently.
+;; There are two commands:
+;;
+;;    neil-quickadd rescan   ; builds the quickadd index from your $SHELL history
+;;    neil-quickadd          ; based on your $SHELL history, add a dep, selecting dep with FZF
+
+(defn read-zshhistory []
+  (let [lines (->> (fs/expand-home "~/.zsh_history")
+                   str
+                   slurp
+                   str/split-lines)
+        neil-lines (filter #(str/includes? % "neil dep add") lines)
+        libs (map (fn [raw-line]
+                    (first
+                     (str/split (str/trim (str/replace raw-line #".*neil dep add" ""))
+                                #"\s+")))
+                  neil-lines)]
+    (->> libs
+         (remove #{"" ":h" "-h" "--help" ":help" "--lib" ":lib"})
+         (into #{})
+         sort)))
+
+(defn read-bashhistory []
+  ;; not yet!
+  (list))
+
+(defn quickadd-rescan [{}]
+  (let [shell (-> (System/getenv "SHELL") (str/split #"/") last)
+        history (cond (= shell "zsh") (read-zshhistory)
+                      (= shell "bash") (read-bashhistory))]
+    (if-let [history (seq history)]
+      (do
+        (fs/create-dirs (fs/expand-home "~/.local/share/neil-quickadd"))
+        (spit (str (fs/expand-home "~/.local/share/neil-quickadd") "/history.edn")
+              (pr-str {:history history})))
+      (do (println "Unable to rescan history! This might be a bug.")
+          (System/exit 1)))))
+
+(defn quickadd [{}]
+  ;; TODO
+  )
+
+(def dispatch-table
+  [{:cmds ["rescan"] :fn quickadd-rescan}
+   {:cmds [] :fn quickadd}])
+
+(defn ensure-env-ok
+  "Terminate and give user error if the user needs to install something."
+  []
+  (when-not (fs/which "fzf")
+    (println "neil-quickadd requires fzf.")
+    (println "Please install fzf, then call `neil-quickadd` again.")
+    (System/exit 1)))
+
+(defn -main [args]
+  (ensure-env-ok)
+  (cli/dispatch dispatch-table args))
