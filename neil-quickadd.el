501c997a603b15a41382e762182b8ea5a3093170
Teodor Heggelund <git@teod.eu>
2023-01-28 13:11:41 +0100

    neil-quickadd-el: add dep

---
+++ neil-quickadd.el
@@ -0,0 +1,32 @@
+;;; neil-quickadd.el -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2023 Teodor Heggelund
+;;
+;; Author: Teodor Heggelund <teodorlu@teod-t490s>
+;; Maintainer: Teodor Heggelund <teodorlu@teod-t490s>
+;; Created: January 28, 2023
+;; Modified: January 28, 2023
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/teodorlu/neil-quickadd
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;; Code:
+
+(require 'projectile)
+(require 's)
+
+(defun neil-quickadd ()
+  (interactive)
+  (let* ((libs (s-lines (s-trim (shell-command-to-string "neil-quickadd libs"))))
+         (selected (completing-read "> " libs)))
+    (projectile-run-shell-command-in-root (s-concat "neil dep add " selected))))
+
+(provide 'neil-quickadd)
+;;; neil-quickadd.el ends here
