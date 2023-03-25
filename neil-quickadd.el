140dd95677cdb304561736b72d7edfae48da0fe3
Teodor Heggelund <git@teod.eu>
2023-03-25 15:40:29 +0100

    Refactor: `neil-quickadd--binary` is private

--- neil-quickadd.el
+++ neil-quickadd.el
@@ -22,23 +22,23 @@
 (require 'projectile)
 (require 's)
 
-(defun neil-quickadd-binary ()
+(defun neil-quickadd--binary ()
   "Which neil binary name to use"
   "neil-quickadd")
 
 (defun neil-quickadd ()
   "Add a deps.edn dependency."
   (interactive)
-  (let* ((libs (s-lines (s-trim (shell-command-to-string (s-concat (neil-quickadd-binary) " libs")))))
+  (let* ((libs (s-lines (s-trim (shell-command-to-string (s-concat (neil-quickadd--binary) " libs")))))
          (selected (completing-read "add lib > " libs)))
     (projectile-run-shell-command-in-root (s-concat "neil dep add " selected))))
 
 (defun neil-quickadd-blacklist ()
   "Blacklist a deps.edn dependency."
   (interactive)
-  (let* ((libs (s-lines (s-trim (shell-command-to-string (s-concat (neil-quickadd-binary) " libs")))))
+  (let* ((libs (s-lines (s-trim (shell-command-to-string (s-concat (neil-quickadd--binary) " libs")))))
          (selected (completing-read "blacklist lib > " libs)))
-    (shell-command-to-string (s-concat (neil-quickadd-binary) " blacklist-lib " selected))))
+    (shell-command-to-string (s-concat (neil-quickadd--binary) " blacklist-lib " selected))))
 
 (defun neil-quickadd-upgrade ()
   "Upgrade deps.edn dependencies."
