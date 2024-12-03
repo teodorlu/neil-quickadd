913d90df7eca4ba1f80c497e71c876f5b90a1ede
Teodor Heggelund <git@teod.eu>
2024-12-03 14:52:17 +0100

    safely-read-edn: cover case where file does not exist

--- test/teodorlu/neil_quickadd_test.clj
+++ test/teodorlu/neil_quickadd_test.clj
@@ -1,6 +1,9 @@
 (ns teodorlu.neil-quickadd-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [deftest is testing]]
             [teodorlu.neil-quickadd :as neil-quickadd]))
 
 (deftest safely-read-edn
-  (is (map? (#'neil-quickadd/safely-slurp-edn "deps.edn" ::fallback))))
+  (testing "File exists"
+    (is (map? (#'neil-quickadd/safely-slurp-edn "deps.edn" ::fallback))))
+  (testing "File does not exist"
+    (is (= ::fallback (#'neil-quickadd/safely-slurp-edn "does-not-exist.edn" ::fallback)))))
